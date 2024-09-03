98ac6af1c2cddf6c73ff36a1b56b9f6af8c90aed
sivakeerth <chandalurikkerthi@gmail.com>
2024-09-03 11:06:32 +0530

    automatic bathroom light on off

---
+++ 4.automatic bathroom light on off/automatic_bathroom_light_on_off1.ino
@@ -0,0 +1,20 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(5,INPUT);
+  pinMode(3,OUTPUT);
+}
+
+void loop()
+{
+  int a= digitalRead(5);
+  if(a==1)
+  {
+  digitalWrite(3,HIGH);
+}
+else
+{
+  digitalWrite(3,LOW);
+}
+}
