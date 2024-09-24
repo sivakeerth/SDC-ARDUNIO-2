8a23feec7b6f6c9d0b6dcec08c08e597c25f7039
sivakeerth <chandalurikkerthi@gmail.com>
2024-09-24 11:06:35 +0530

    lcdi2cardunio

---
+++ 13.lcdi2cardunio/13.lcdi2cardunio.ino
@@ -0,0 +1,21 @@
+// C++ code
+//
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x26,16,2);
+void setup()
+{
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.setBacklight(HIGH);
+  lcd.setCursor(0,1);
+  lcd.print("abc");
+  lcd.setCursor(0,0);
+  lcd.print("def");
+  
+}
+
+void loop()
+{
+  
+}
